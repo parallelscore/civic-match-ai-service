a5e1417d89d3711e31e7516be5cbd7b58ad6e26c
Marvellous <akomolafemarvellous@gmail.com>
2025-05-16 10:08:46 +0100

    Update test runner to exclude specific files and directories from coverage

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the tests with coverage
-coverage run --source=. -m pytest tests/ -vv
+coverage run --source=. --omit="*/tests/*,*/migrations/*,*/__pycache__/*,*/venv/*,t_matching_engine.py,t_matching_engine_2.py" -m pytest tests/ -vv
 
 # Generate an HTML report
 coverage html
