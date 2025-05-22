f310f9896d486a8e43b824277d56a10f509eecc2
Marvellous <akomolafemarvellous@gmail.com>
2025-05-23 00:11:30 +0100

    Update logging in submit_voter_responses and adjust test endpoint URL

--- t_matching_engine.py
+++ t_matching_engine.py
@@ -7,8 +7,8 @@ with open('test_voter_submission.json', 'r') as f:
     test_data = json.load(f)
 
 # Send the request
-response = requests.post('http://localhost:8000/api/v1/matching_engine', json=test_data)
-# response = requests.post('https://elected-ai.parallelscore.com/api/v1/matching_engine', json=test_data)
+# response = requests.post('http://localhost:8000/api/v1/matching_engine', json=test_data)
+response = requests.post('https://elected-ai.parallelscore.com/api/v1/matching_engine', json=test_data)
 
 # Print the response
 print(f"Status code: {response.status_code}")
